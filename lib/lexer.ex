a4197be0a0f204cc88285a6a5b51309b4bc365e8
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2019-05-15 07:01:07 -0400

    super basic tokenization of strings and assignments

--- lib/lexer.ex
+++ lib/lexer.ex
@@ -9,9 +9,34 @@ defmodule Amethyst.Lexer do
 
     amethyst_src
     |> String.trim() # Trim entire source leading + trailing whitespace
-    |> String.split("\n") # Split into array of lines
-    |> Enum.map(&String.trim/1) # Trim each line of the source
+    |> create_tokens()
     |> IO.inspect()
+  end
+
+  defp create_tokens(src, tokens \\ [])
+
+  defp create_tokens(<<>>, tokens) do
+    tokens
+    |> Enum.filter(& &1 != :whitespace)
+    |> Enum.reverse()
+  end
+
+  defp create_tokens(<<char::bytes-size(1), src::binary>>, tokens) do
+    # {type, value}
+
+    {token, src} =
+      cond do
+        char in [" ", "\n"] -> {:whitespace, src}
+        char == "=" -> {:assignment, src}
+        char in ["\"", "'"] ->
+          [string, rest] = String.split(src, char, parts: 2)
+          {{:string, string}, rest}
+        Regex.match?(~r/[a-zA-Z]/, char) ->
+          [symbol, rest] = String.split(src, ~r/[^a-zA-Z0-9_$]/, parts: 2)
+          {{:symbol, char <> symbol}, rest}
+      end
+
+    create_tokens(src, [token | tokens])
   end
 
 end
